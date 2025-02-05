45f604f0427ffeb89d9408b68f90a02f5490cb8a
Jonathan Eliah Amdam <jonathan@amdam.com>
2025-02-05 14:29:45 +0100

    Fixed port

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ app.use((req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5001; // Use $PORT on Heroku, default to 5001 locally
+const PORT = process.env.PORT || 5000; // Use $PORT on Heroku, default to 5001 locally
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
