c84c5868b32661b9d572027019c755eb830c20bc
Jonathan Eliah Amdam <jonathan@amdam.com>
2025-02-10 19:04:31 +0100

    The right folder this time

--- server/Controller.js
+++ server/Controller.js
@@ -20,7 +20,7 @@ class Controller {
             throw new Error("Passwords do not match");
         }
         const user = await this.agentDAO.registerUser(firstName, lastName, personNumber, username, email, password, role_id);
-        
+    
         if (user) {
             const { password, ...userData } = user.dataValues;
             console.log(userData)
