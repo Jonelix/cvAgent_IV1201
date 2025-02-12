1ee6cab8cee8d26952a3c65e51a4ba2ceaacf4ee
ziangw2001 <ziangwang2001@gmail.com>
2025-02-12 14:58:59 +0100

    test

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 const requestHandler = new RequestHandler();
 requestHandler.initializeRoutes(app);
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
     console.log(`Server up at http://localhost:${PORT}`);
 });
