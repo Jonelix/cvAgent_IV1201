1208cd9eeff936965f0211fabf7a69bd2bb49156
ziangw2001 <ziangwang2001@gmail.com>
2025-02-13 15:16:31 +0100

    Remove some unused file

--- client/src/presenters/DashboardPresenter 2.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { observer } from "mobx-react-lite";
-import DashboardView from "../views/DashboardView.jsx";
-
-const DashboardPresenter = observer(({ model }) => {
-    return <DashboardView model={model} />;
-});
-
-export default DashboardPresenter;
