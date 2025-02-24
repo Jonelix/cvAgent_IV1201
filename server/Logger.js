f17a8b8d06c7f95b88bbb7689aa5304ecc61cc9c
Jonathan Eliah Amdam <jonathan@amdam.com>
2025-02-24 13:07:14 +0100

    Logger logs

--- server/Logger.js
+++ server/Logger.js
@@ -1,14 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
 class Logger {
-  constructor() {
-    this.logStream = fs.createWriteStream(path.join(__dirname, 'app.log'), { flags: 'a' });
-  }
-
   log(message) {
     const timestamp = new Date().toISOString();
-    this.logStream.write(`[${timestamp}] ${message}\n`);
+    console.log(`[${timestamp}] ${message}`);
   }
 }
 
