c9c4869dbf37ee56d52bd159a43dd2d985e560a2
Jonathan Eliah Amdam <jonathan@amdam.com>
2025-02-26 14:31:47 +0100

    Validation functions for registration

--- server/Validation.js
+++ server/Validation.js
@@ -1,6 +1,28 @@
 class Validation {
+    //WILL RETURN TRUE IF THERE ARE NO ERRORS
+
+    //Name validation
+    static validateName(name) {
+        return /^[A-Za-z]{2,255}$/.test(name);
+    }
+
+    static validateUsername(username) {
+        return /^[A-Za-z0-9]{6,255}$/.test(username);
+    }
+
+    //Password validation
+    static validatePassword(password) {
+        return /^(?=.*[A-Z])(?=.*\d)(?=.*[!@$%^&*+#])[A-Za-z\d!@$%^&*+#]{6,255}$/.test(password);
+    }
+
+    //PNR validation
+    static validatePNR(pnr) {
+        return /^\d{12}$/.test(pnr);
+    }
+
+    //Email validation
     static validateEmail(email) {
-        return email.includes('@');
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
     }
 }
 
